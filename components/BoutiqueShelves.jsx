b9fb6307fdd063ccb2f119fe05e584f1a63376e0
stephanieshelbig <stephanieshelbig@gmail.com>
2025-08-13 10:35:58 -0700

    Update BoutiqueShelves.jsx

--- components/BoutiqueShelves.jsx
+++ components/BoutiqueShelves.jsx
@@ -11,7 +11,7 @@ const SHELF_CENTER_X = { left: 31.8, center: 50.0, right: 68.2 };
 const COLUMNS = ['left', 'center', 'right'];
 
 /** Bottle heights */
-const H_DESKTOP = 120, H_TABLET = 100, H_MOBILE = 84;
+const H_DESKTOP = 90, H_TABLET = 75, H_MOBILE = 64;
 
 function getBottleH() {
   if (typeof window === 'undefined') return H_DESKTOP;
