b7e9d76f7353a2343d9d3233d974882ce1afa029
stephanieshelbig <stephanieshelbig@gmail.com>
2025-08-20 20:28:23 -0700

    Update isAdmin.ts

--- lib/isAdmin.ts
+++ lib/isAdmin.ts
@@ -1,5 +1,5 @@
 export const ADMIN_EMAILS = new Set<string>([
-  'stephanie@fragrantique.net', // you can add more if needed
+  'stephanieshelbig@gmail.com', // you can add more if needed
 ]);
 
 export function isAdminEmail(email?: string | null) {
