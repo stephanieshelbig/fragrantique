b9bba7f858f704e54eeccb3ca6fbdcb78d72b5bd
stephanieshelbig <stephanieshelbig@gmail.com>
2025-08-25 10:14:43 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-[#fdfcf9] border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        {/* Brand logo */}
+        {/* Brand logo / home link */}
         <Link href="/" className="text-2xl font-bold text-[#b5985a]">
           Fragrantique
         </Link>
