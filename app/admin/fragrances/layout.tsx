98034a4513674d38b6e0121a8f5508fe58e42b17
stephanieshelbig <stephanieshelbig@gmail.com>
2025-09-22 11:46:10 -0700

    Create layout.tsx

---
+++ app/admin/fragrances/layout.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = false;
+
+export default function AdminFragrancesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
