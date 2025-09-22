6a1c42daae320280275a0a0f0856ad893243fdfc
stephanieshelbig <stephanieshelbig@gmail.com>
2025-09-22 11:39:28 -0700

    Update page.jsx

--- app/admin/fragrances/page.jsx
+++ app/admin/fragrances/page.jsx
@@ -1,5 +1,6 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
+export const revalidate = false;
 export const dynamic = 'force-dynamic';
 
 import { useEffect, useMemo, useState, useTransition } from 'react';
