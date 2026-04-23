433a86b3f6b212764aed22ed39da5bbff5727e9d
stephanieshelbig <stephanieshelbig@gmail.com>
2026-04-23 11:34:29 -0700

    Update HeaderBar.tsx

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -23,7 +23,7 @@ export default function HeaderBar() {
             Musk Anosmia
           </Link>
           <Link href="/photos" className="hover:underline">
-            Pics of My Collection
+            📸Collection Pics
           </Link>
           <Link href="/requests" className="hover:underline">
             🆕Request a Fragrance
