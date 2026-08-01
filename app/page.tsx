427a100d4e160ffbd739c37a8e9372c354f7fbcc
stephanieshelbig <stephanieshelbig@gmail.com>
2026-08-01 11:15:03 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -134,9 +134,9 @@ export default function HomePage() {
             </h1>
 
             <p className="text-lg md:text-xl text-[#4b5360] leading-9 font-light italic max-w-2xl mx-auto">
-              Welcome to Fragrantique, where you can explore my personal fragrance collection and purchase affordable luxury decants. Start browsing below and find your next favorite scent. Feel free to contact me
-              using the <span className="font-semibold">'Contact Me'</span> link at the top
-              of the page💕
+              Thank you so much for viewing my page! I have a large fragrance collection,
+              and I sell decants of it so everyone can enjoy. Click one
+              of the buttons below to start browsing my collection💕
             </p>
           </div>
 
